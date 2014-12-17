85bd6b248a5c1a4f329026a85183d24422a16195
leonardocarvalho <leonardo.carvalho@eurekaeducacional.com.br>
2014-12-17 10:12:36 -0200

    Helper decorator to validate requests using json schema

---
+++ glibs-jsonschema/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+
+setuptools.setup(
+    name="geekie-jsonschema",
+    version="1.0",
+    url="http://www.geekie.com.br",
+    maintainer="Geekie",
+    maintainer_email="geekie@geekie.com.br",
+    packages=["geekie"],
+    namespace_packages=["geekie"],
+    include_package_data=True,
+    zip_safe=False,
+    setup_requires=["setuptools_git==1.0b1"],
+    install_requires=[
+        "jsonschema>=2.4.0",
+    ],
+    extras_require={
+        "testing": [
+            "mock==1.0.1",
+        ],
+    }
+)
